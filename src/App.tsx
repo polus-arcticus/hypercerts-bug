423a29073782718cff2928c8a88ec7720d3762bf
Taylor Hulsmans <thulsmans.1133@gmail.com>
2023-11-22 12:21:24 -0800

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -5,41 +5,41 @@ import { InjectedConnector } from 'wagmi/connectors/injected'
 import { HypercertClient  } from "@hypercerts-org/sdk";
 
 function App() {
-	const { address, isConnected } = useAccount()
-	const { data: ensName } = useEnsName({ address })
-	const { connect } = useConnect({
-		connector: new InjectedConnector(),
-	})
-	const publicClient = usePublicClient()
+  const { address, isConnected } = useAccount()
+  const { data: ensName } = useEnsName({ address })
+  const { connect } = useConnect({
+    connector: new InjectedConnector(),
+  })
+  const publicClient = usePublicClient()
 
-	const { data: walletClient, isError, isLoading  } = useWalletClient()
+  const { data: walletClient, isError, isLoading  } = useWalletClient()
 
 
 
-	useEffect(() => {
-		if (walletClient && publicClient && isConnected) {
-			const hypercertClient = new HypercertClient({
-				chain: {
-					id: 10,
-				}, // optimism
+  useEffect(() => {
+    if (walletClient && publicClient && isConnected) {
+      const hypercertClient = new HypercertClient({
+        chain: {
+          id: 10,
+        }, // optimism
         easContractAddress: '0x4200000000000000000000000000000000000021',
-				walletClient,
-				publicClient,
-				nftStorageToken: '...please paste an nft storage token here...',
-			})
-		}
+        walletClient,
+        publicClient,
+        nftStorageToken: '...please paste an nft storage token here...',
+      })
+    }
 
-	}, [address, walletClient, publicClient, isConnected])
+  }, [address, walletClient, publicClient, isConnected])
 
-	return (
-		<>
-			{isConnected ? (<div>Connected to {ensName ?? address}</div>):
-				(<button onClick={() => connect()}>Connect Wallet</button>)
-			}
+  return (
+    <>
+      {isConnected ? (<div>Connected to {ensName ?? address}</div>):
+        (<button onClick={() => connect()}>Connect Wallet</button>)
+      }
 
 
-			</>
-	)
+      </>
+  )
 }
 
 export default App
